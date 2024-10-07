9f5c12df36684013d371072a41544b724e21bedd
Monica <momoclaro@gmail.com>
2024-10-07 11:53:25 +0200

    exercises lecture 2

---
+++ OneDrive/Desktop/Python/after-class-exercises/README.md
@@ -0,0 +1,6 @@
+# after-lecture-exercises
+the exercises of the first lecture have been added.
+
+With regards to exercise 11 .upper() was used to ensure that letters within the decided range (a to f) even if inputted in lower cases would be accepted by the code.
+
+For exercise 17 the first numbers are being transformed into a float as they would otherwise be stored as strings by the code and be unable to process for the mathematical operations.
--- OneDrive/Desktop/Python/after-class-exercises/exercises lecture 1.py
+++ OneDrive/Desktop/Python/after-class-exercises/exercises lecture 1.py
@@ -63,3 +63,71 @@ while i<6:
     i+=1
 
 #exercise 11
+grade=input('Input a grade: ').upper()
+match grade:
+    case 'A':
+        print('Excellent!')
+    case 'B':
+        print('Good job!')
+    case 'C':
+        print('Fair')
+    case 'D':
+        print('Needs improvement')
+    case 'F':
+        print('Failing')
+    case _:
+        print('You did not enter a grade, please retry')
+
+#exercise 12
+def greet(name):
+    print(f'Hello, {name}!')
+greet('Monica')
+
+#exercise 13
+def square(number):
+    return number**2
+
+print(square(3))
+print(square(7))
+print(square(9))
+
+
+#exercise 14
+def multiply(a,b=1):
+    return a*b 
+
+print(multiply(3,2))
+print(multiply(3))
+
+#exercise 15
+num_list=[l for l in range(1,11)]
+new_list=[]
+for item in num_list:
+    new_list.append(item**2)
+print(new_list)
+
+#exercise 16
+student_grades={'Azriel':[9,7.5,8,8.7],
+                'Feyre':[6,6.4,7,9.2],
+                'Rhysand':[9.8,8.1,9.3,10]}
+for student in student_grades:
+    average=sum(student_grades[student])/len(student_grades[student])
+    print(f'{student} average grade is {average}')
+
+#exercise 17
+def calculate(num1,num2,operator):
+    if operator== '+':
+        return num1 + num2
+    elif operator== '-':
+        return num1 - num2
+    elif operator== '*':
+        return num1 * num2
+    elif operator== '/':
+        return num1 / num2
+    else:
+        print('invalid operator, please retry with +,-,* or /')
+first=float(input('please input the first number: '))
+second=float(input('please input the second number: '))
+op=input('please input what operator would you like to use: ')
+
+calculate(first,second,op)
